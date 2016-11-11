065c8dba957e7c9a92fb711115f3bf83261c1b18
Avanrethus <kazuhi13@gmail.com>
2016-11-11 13:58:55 +0100

    Aktualizacje.

--- FabrykaStron/src/FabrykaMain/Fabryka.java
+++ FabrykaStron/src/FabrykaMain/Fabryka.java
@@ -1,6 +1,25 @@
 package FabrykaMain;
+import Strony.*;
 
 public class Fabryka {
+    public tworzStrony getRodzaj(String rodzajStrony){
+        if(rodzajStrony == null){
+            return null;
+        }
+        if(rodzajStrony.equalsIgnoreCase("GaleriaZdjec")){
+            return new GaleriaZdjec();
+        }
+        if(rodzajStrony.equalsIgnoreCase("Informacyjna")){
+            return new Informacyjna();
+        }
+        if(rodzajStrony.equalsIgnoreCase("Kontaktowa")){
+            return new Kontaktowa();
+        }
+        else if(rodzajStrony.equalsIgnoreCase("Aktualnosci")){
+            return new Aktualnosci();
+        }
+        return null;
+    }
     public void tworzStrone(){
             
     }
--- FabrykaStron/src/FabrykaMain/Program.java
+++ FabrykaStron/src/FabrykaMain/Program.java
@@ -1,5 +1,21 @@
 package FabrykaMain;
 
 public class Program {
-    
+    public static void main(String[] args){
+        Fabryka fab = new Fabryka();
+        
+        tworzStrony galeriaZdjec = fab.getRodzaj("GaleriaZdjec");
+        galeriaZdjec.tworz();
+        
+        tworzStrony info = fab.getRodzaj("Informacyjna");
+        info.tworz();
+        
+        tworzStrony kontaktowa = fab.getRodzaj("Kontaktowa");
+        kontaktowa.tworz();
+        
+        tworzStrony aktualnosci = fab.getRodzaj("Aktualnosci");
+        aktualnosci.tworz();
+        
+    }
 }
+        
